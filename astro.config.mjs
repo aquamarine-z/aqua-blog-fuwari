bb119c366d97ba0da4cf7a1895269cb44e45ba87
24180 <2418013844@qq.com>
2026-06-14 09:07:55 +0800

    fix(mobile): move mobile toc outside swup container to fix positioning

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,7 +53,7 @@ export default defineConfig({
 			animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
 			// the default value `transition-` cause transition delay
 			// when the Tailwind class `transition-all` is used
-			containers: ["main", "#toc", "#sidebar", "#sidebar-bottom"],
+			containers: ["main", "#toc", "#sidebar", "#sidebar-bottom", "#mobile-toc-container"],
 			smoothScrolling: true,
 			cache: true,
 			preload: true,
