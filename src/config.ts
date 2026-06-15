c1e79594c931a0fea4e3f4fe9eb3067fe0cb641e
Aquamarine <2418013844@qq.com>
2026-06-15 19:26:57 +0800

    fix: update site title to Aqua and change favicon to avatar

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
+	title: "Aqua",
 	subtitle: 'The French version of Fuwari',
 	lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th', etc.
 	languages: ["zh_CN","ja", "ko", "en", "fr" ], // Add this array to precisely match language prefixes
@@ -33,12 +33,9 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: '/favicon.png'
+		}
 	],
 };
 
